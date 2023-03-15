401bff46e4e6e894041719bc66250d2544690259
Joan D <joandidachr@gmail.comm>
2023-03-15 11:03:58 +0100

    Adds iteration 3

--- src/movies.js
+++ src/movies.js
@@ -2,64 +2,50 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {
-let result = moviesArray.map(moviesArray => movies.director);
 
-//if (movies.length > result.length ) {result.push(movies.director)}
-return result
-}
+
+// function getAllDirectors(movies) {
+//     return movies.map(movie => movie.director);
+//   }
+function getAllDirectors(movies) {
+    const directors = movies.map(movie => movie.director);
+    return [...new Set(directors)];
+  }
+  
+  
 
 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {
-
-    let searchDrama = 'Drama';
-    // const totalDrama =  movies.genre.find(genres => genres.includes(searchDrama));
-    // console.log(totalDrama)
-    let howMany = 0;
-    for (let i=0 ; i < moviesArray.length ; i++) {
-
-        if (moviesArray.length === 0 ) {
-            return 0}
-
-             
-         if (movies.director === 'Steven Spielberg' && movies.genre[0] === 'Drama'||
-             movies.director === 'Steven Spielberg' && movies.genre[1] === 'Drama'||
-             movies.director === 'Steven Spielberg' && movies.genre[2] === 'Drama'||
-             movies.director === 'Steven Spielberg' && movies.genre[3] === 'Drama') {
-            howMany++;
-            console.log(howMany);
-         }
-
-            // const result = movies.map(genres => genres === 'Drama' ? totalDrama[counter++] : genres);
-            // console.log(result);
+function howManyMovies(movies) {
+    const filteredMovies = movies.filter(movie => 
+    ( movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')));
+    return filteredMovies.length;
+  }
 
 
-            // if (movies.director === 'Steven Spielberg' && movies.genre[0][1][2] === 'Drama') {
-            // howMany++;
-            // console.log(howMany);
-            
-            // } if ( movies.genre.find("Drama")) {
-            // drama++;
-    }
-    return howMany
-}
-
-
-       
-
-
-    
-
-
-
-
+  
 
 
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0) {
+      return 0;
+    }
+  
+    const scores = moviesArray.reduce((total, movie) => {
+      if (typeof movie.score === 'number') {
+        return total + movie.score;
+      } else {
+        return total;
+      }
+    }, 0);
+  
+    const average = scores / moviesArray.length;
+    return parseFloat(average.toFixed(2));
+  }
+  
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
